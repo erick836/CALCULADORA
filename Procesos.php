e981eb9f2cbd04edd94a9a0325e5cbeebac788b5
Erick Alexander Chávez González <201800153@colsanjose.edu.gt>
2020-04-04 12:56:17 -0600

    CONDICIONAL SUMA Y RESTA

--- Procesos.php
+++ Procesos.php
@@ -1,9 +1,13 @@
 <?php 
 $VALOR1=$_POST['N1'];
 $VALOR2=$_POST['N2'];
+$OPERACION=$_POST['OPERACION'];
 
-	echo "LA SUMA ES:".($VALOR1+$VALOR2);
-
-
+if ($OPERACION == 'SUMA') {
+	echo "LA SUMA ES: ".($VALOR1+$VALOR2);
+}
+if ($OPERACION == 'RESTA') {
+	echo "LA RESTA ES: ".($VALOR1-$VALOR2);
+}
 
 ?>
