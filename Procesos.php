74ca7a72e69cb5ffe5b34a0f1d95c18382bbce6e
amilc <amilc@LAPTOP-L9O0VFRL>
2020-04-04 12:57:01 -0600

    Agregue multiplicacion y division

--- Procesos.php
+++ Procesos.php
@@ -1,5 +1,23 @@
 <?php 
 $VALOR1=$_POST['N1'];
 $VALOR2=$_POST['N2'];
+
+$OPERACION=$_POST['OPERACION'];
+
+if($OPERACION =='SUMA')
+{
 echo "La suma es:".($VALOR1+$VALOR2);
+}
+else if($OPERACION =='RESTA')
+{
+echo "La resta es:".($VALOR1-$VALOR2);
+}
+else if($OPERACION =='MULTIPLICACION')
+{
+echo "La multiplicacion es:".($VALOR1*$VALOR2);
+}
+else if($OPERACION =='DIVISION')
+{
+echo "La division es:".($VALOR1/$VALOR2);
+}
 ?>
